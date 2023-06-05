2dfe9ee5cae2edd886f97b4274c9e3f262f312a8
Emre <emrekavak3938@gmail.com>
2023-06-05 15:47:58 +0100

    Initial commit

---
+++ src/main/java/com/planner/journeyplanner/JourneyplannerApplication.java
@@ -0,0 +1,13 @@
+package com.planner.journeyplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planner/journeyplanner/JourneyplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planner.journeyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
