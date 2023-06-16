d84fcc8c8776496ad7fc77c9347eab70aac09e44
Emre <emrekavak3938@gmail.com>
2023-06-16 21:08:54 +0100

    method improve
    methods are divided sub method in order maintain clean and maintainable code

--- src/main/java/com/planner/journeyplanner/service/GptService.java
+++ src/main/java/com/planner/journeyplanner/service/GptService.java
@@ -1,7 +1,9 @@
 package com.planner.journeyplanner.service;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.net.URL;
 
 import com.planner.journeyplanner.config.AppConfig;
@@ -29,119 +31,88 @@ public class GptService {
     public void init() {
         API_KEY = config.getProperty("CHATGPT_API_KEY");
     }
-
-
     public JSONObject sendMessageToGpt(String text) throws Exception {
-        String url = "https://api.openai.com/v1/completions";
+        String url = "https://api.openai.com/v1/completions"; //"https://api.openai.com/v1/chat/completions"
         int attempts = 0;
         long waitTime = INITIAL_WAIT_TIME_MS;
-        text = "Can you analyze the text below and provide the origin and destination cities in the following format? 'Origin: <origin city>, Destination: <destination city>'. " + "\n" + text;
+        text = buildPrompt(text);
 
         while (attempts <= MAX_RETRIES) {
-            HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
-
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Content-Type", "application/json");
-            con.setRequestProperty("Authorization", API_KEY);
-
-            JSONObject data = new JSONObject();
-            data.put("model", "text-davinci-003");
-            data.put("prompt", text);
-            data.put("max_tokens", 4000);
-            data.put("temperature", 1.0);
-
-            con.setDoOutput(true);
-            con.getOutputStream().write(data.toString().getBytes());
+            HttpURLConnection con = createConnection(url);
+            setRequestProperties(con);
+            sendRequestData(con, text);
 
             int responseCode = con.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuffer content = new StringBuffer();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                in.close();
-
-                String gptResponse = new JSONObject(content.toString()).getJSONArray("choices").getJSONObject(0).getString("text");
-                System.out.println(gptResponse);
-                // Parse the response string
-                String[] parts = gptResponse.split(", ");
-                if (parts.length == 2) {
-                    String originCity = parts[0].split(": ")[1];  // "Ankara"
-                    String destinationCity = parts[1].split(": ")[1];  // "Sivas"
-
-                    // Construct a new JSON object
-                    JSONObject journey = new JSONObject();
-                    journey.put("origin", originCity);
-                    journey.put("destination", destinationCity);
-
-                    return journey;
-                } else {
-                    // Handle the case when GPT-3 response does not contain origin and destination lines
-                    throw new Exception("Unexpected GPT-3 response format");
-                }
+                String gptResponse = readResponse(con);
+                return parseResponse(gptResponse);
             } else if (responseCode == 429) {
-                System.out.println("Rate limit reached, waiting for " + waitTime + "ms before retrying...");
-                Thread.sleep(waitTime);
-                attempts++;
-                waitTime *= 2;
+                handleRateLimitExceeded(waitTime, attempts++);
             } else {
                 throw new Exception("Received HTTP status code " + responseCode + " from API.");
             }
         }
-
         throw new Exception("Failed to get a successful response after " + MAX_RETRIES + " attempts.");
     }
 
+    private String buildPrompt(String text) {
+        return "Can you analyze the text below and provide the origin and destination cities in the following format? 'Origin: <origin city>, Destination: <destination city>'. " + "\n" + text;
+    }
 
-    /*public String sendMessageToGpt(String text) throws Exception {
-        String url = "https://api.openai.com/v1/completions";
-        int attempts = 0;
-        long waitTime = INITIAL_WAIT_TIME_MS;
+    private HttpURLConnection createConnection(String url) throws IOException {
+        return (HttpURLConnection) new URL(url).openConnection();
+    }
 
-        while (attempts <= MAX_RETRIES) {
-            HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+    private void setRequestProperties(HttpURLConnection con) throws ProtocolException {
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Authorization", API_KEY);
+    }
 
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Content-Type", "application/json");
-          //  System.out.println("athe time executin" + API_KEY);
-            con.setRequestProperty("Authorization", API_KEY);
+    private void sendRequestData(HttpURLConnection con, String text) throws IOException {
+        JSONObject data = new JSONObject();
+        data.put("model", "text-davinci-003");   //  text-davinci-003  gpt-3.5-turbo-0613
+        data.put("prompt", text);
+        data.put("max_tokens", 4000);
+        data.put("temperature", 1.0);
+        con.setDoOutput(true);
+        con.getOutputStream().write(data.toString().getBytes());
+    }
 
-            JSONObject data = new JSONObject();
-            data.put("model", "text-davinci-003");
-            data.put("prompt", text);
-            data.put("max_tokens", 4000);
-            data.put("temperature", 1.0);
-
-            con.setDoOutput(true);
-            con.getOutputStream().write(data.toString().getBytes());
-
-            int responseCode = con.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuffer content = new StringBuffer();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                in.close();
-
-                System.out.println(new JSONObject(content.toString()).getJSONArray("choices").getJSONObject(0).getString("text"));
-                String togo = (new JSONObject(content.toString()).getJSONArray("choices").getJSONObject(0).getString("text"));
-
-                return togo;
-            } else if (responseCode == 429) {
-                System.out.println("Rate limit reached, waiting for " + waitTime + "ms before retrying...");
-                Thread.sleep(waitTime);
-                attempts++;
-                waitTime *= 2;
-            } else {
-                throw new Exception("Received HTTP status code " + responseCode + " from API.");
-            }
+    private String readResponse(HttpURLConnection con) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
         }
+        in.close();
+        System.out.println(content.toString());
+        return new JSONObject(content.toString()).getJSONArray("choices").getJSONObject(0).getString("text");
+    }
 
-        throw new Exception("Failed to get a successful response after " + MAX_RETRIES + " attempts.");
-    }*/
+    private JSONObject parseResponse(String gptResponse) throws Exception {
+        // Parse the response string
+        String[] parts = gptResponse.split(", ");
+        if (parts.length == 2) {
+            String originCity = parts[0].split(": ")[1];  // "Ankara"
+            String destinationCity = parts[1].split(": ")[1];  // "Sivas"
+
+            // Construct a new JSON object
+            JSONObject journey = new JSONObject();
+            journey.put("origin", originCity);
+            journey.put("destination", destinationCity);
+            return journey;
+        } else {
+            // Handle the case when GPT-3 response does not contain origin and destination lines
+            throw new Exception("Unexpected GPT-3 response format");
+        }
+    }
+
+    private void handleRateLimitExceeded(long waitTime, int attempts) throws InterruptedException {
+        System.out.println("Rate limit reached, waiting for " + waitTime + "ms before retrying...");
+        Thread.sleep(waitTime);
+        waitTime *= 2;
+    }
 
 }
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,44 +1,79 @@
 let map, directionsService, directionsRenderer;
 
+// function to handle button click event
+function handleButtonClick() {
+    console.log("Button was clicked");
+    var userInput = getUserInput();
+    processUserInput(userInput);
+}
 
-document.getElementById('send-button').addEventListener('click', function() {
-  console.log("Button was clicked");
-  var userInput = document.getElementById('user-input').value;
-  console.log(userInput);
+// function to get user input
+function getUserInput() {
+    var userInput = document.getElementById('user-input').value;
+    console.log(userInput);
+    return userInput;
+}
 
-  fetch('http://localhost:8082/api/journey', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        message: userInput
+// function to process user input
+function processUserInput(userInput) {
+    fetch('http://localhost:8082/api/journey', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            message: userInput
+        })
     })
-  })
- .then(response => response.text()) // Get the response body as text
- .then(text => {
-     let data = JSON.parse(text); // Parse the text into a JavaScript object
-
-     console.log("Origin: " + data.origin);
-     console.log("Destination: " + data.destination);
-
-     let request = {
-       origin: data.origin,
-       destination: data.destination,
-       travelMode: 'DRIVING'
-     };
-
-
-    directionsService.route(request, function(result, status) {
-      if (status == 'OK') {
-        // Set the directions on the map
-        directionsRenderer.setDirections(result);
-      } else {
-        console.error('Directions request was not successful for the following reason: ' + status);
-      }
+    .then(response => response.text()) // Get the response body as text
+    .then(data => processText(data)) // Call the processText function with the data
+    .catch((error) => {
+        console.error('Error:', error);
     });
-  })
-  .catch((error) => {
-      console.error('Error:', error);
-  });
+}
+
+// function to process the text from the response
+function processText(text) {
+    let data = JSON.parse(text);
+
+    console.log("Origin: " + data.origin);
+    console.log("Destination: " + data.destination);
+
+    let request = {
+        origin: data.origin,
+        destination: data.destination,
+        travelMode: 'DRIVING'
+    };
+
+    routeDirection(request);
+}
+
+// function to route direction
+function routeDirection(request) {
+    directionsService.route(request, function(result, status) {
+    //console.log(result);
+        if (status == 'OK') {
+            // Set the directions on the map
+            directionsRenderer.setDirections(result);
+        } else {
+            console.error('Directions request was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+// Add event listener to the button
+document.getElementById('send-button').addEventListener('click', handleButtonClick);
+// Get your input field
+var inputField = document.getElementById('user-input');
+
+// Listen for keydown event
+inputField.addEventListener('keydown', function(event) {
+    // Check if the key is the "Enter" key
+    if (event.key === "Enter") {
+        // Prevent the default action to stop the form from submitting
+        event.preventDefault();
+        // Call your function
+        handleButtonClick();
+    }
 });
+
