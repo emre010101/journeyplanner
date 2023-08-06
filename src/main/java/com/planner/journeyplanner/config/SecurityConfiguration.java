3255c11c209c81a86058b76933499058ab2fe328
Emre <emrekavak3938@gmail.com>
2023-08-06 13:09:36 +0100

    cors wild card

--- src/main/java/com/planner/journeyplanner/config/SecurityConfiguration.java
+++ src/main/java/com/planner/journeyplanner/config/SecurityConfiguration.java
@@ -17,6 +17,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -59,7 +60,7 @@ public class SecurityConfiguration {
     }
 
     //Configuring cors policy to allow our front end
-    @Bean
+   /* @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
@@ -69,6 +70,16 @@ public class SecurityConfiguration {
                 "https://emre010101.github.io/journeyplannerfront",
                 "https://polite-river-0dc0e4003.3.azurestaticapps.net"
         ));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }*/
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));  // Set wildcard for allowed origins
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
